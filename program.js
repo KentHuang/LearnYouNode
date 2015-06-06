ffc3911be4050d0fa300f282c49b83abd8b3354f
KentHuang <kh395@cornell.edu>
2015-06-05 23:12:06 -0400

    file server

--- program.js
+++ program.js
@@ -1,26 +1,7 @@
 var http = require('http');
-var bl = require('bl');
-var results = [];
-var count = 0;
+var fs = require('fs');
 
-function foo (i) {
-  http.get(process.argv[2+i], function (res) {
-    res.pipe(bl(function(err, data) {
-      if (err) { throw err; }
-      //console.log(data.toString());
-      results[i] = data.toString();
-      count++;
-
-      if (count === 3) {
-        for (var j = 0; j < 3; j++) {
-          console.log(results[j]);
-        }
-      }
-    }));
-  });
-}
-
-for (var i = 0; i < 3; i++) { 
-  foo(i)
-}
-
+var server = http.createServer(function (req, res) {
+  fs.createReadStream(process.argv[3]).pipe(res);
+});
+server.listen(process.argv[2]);
