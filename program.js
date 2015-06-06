a813ecc81977a2be565a87cbe00a26d20dafcbb7
KentHuang <kh395@cornell.edu>
2015-06-05 21:23:05 -0400

    async

--- program.js
+++ program.js
@@ -1,11 +1,26 @@
-var mod = require('./module.js');
+var http = require('http');
+var bl = require('bl');
+var results = [];
+var count = 0;
 
-mod(process.argv[2], process.argv[3], function(err, data) {
-  if(err) {
-    throw err;
-  } else {
-    for (var i = 0; i < data.length; i = i + 1) {
-      console.log(data[i]);
-    };
-  }
-});
+function foo (i) {
+  http.get(process.argv[2+i], function (res) {
+    res.pipe(bl(function(err, data) {
+      if (err) { throw err; }
+      //console.log(data.toString());
+      results[i] = data.toString();
+      count++;
+
+      if (count === 3) {
+        for (var j = 0; j < 3; j++) {
+          console.log(results[j]);
+        }
+      }
+    }));
+  });
+}
+
+for (var i = 0; i < 3; i++) { 
+  foo(i)
+}
+
