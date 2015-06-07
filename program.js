7d1dcf6349135fb0d592567bc6023895c49d14f1
KentHuang <kh395@cornell.edu>
2015-06-07 05:13:01 -0400

    JSON server

--- program.js
+++ program.js
@@ -1,7 +1,19 @@
 var http = require('http');
-var fs = require('fs');
 
 var server = http.createServer(function (req, res) {
-  fs.createReadStream(process.argv[3]).pipe(res);
+  var url_parsed = require('url').parse(req.url, true); 
+  var pname = url_parsed.pathname;
+  var pdate =  new Date(url_parsed.query.iso);
+  var obj = {};
+  if (pname === '/api/parsetime') {
+    obj = { 'hour': pdate.getHours(), 
+            'minute': pdate.getMinutes(), 
+            'second': pdate.getSeconds() };
+  } else if (pname === '/api/unixtime') {
+    obj =  { 'unixtime': pdate.getTime() };
+  }
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  res.end(JSON.stringify(obj));
 });
+
 server.listen(process.argv[2]);
